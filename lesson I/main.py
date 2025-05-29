0996b301e1bc991cda031e11201c0e63df475326
Gauthamsenthil <gauthamsenthilraja@gmail.com>
2025-05-29 20:41:21 +0530

    submit

---
+++ lesson I/main.py
@@ -0,0 +1,11 @@
+#printing a greeting message
+print("hello world!")
+
+#printing my name
+print("My name is Gautham.")
+
+#printing a fun fact about myself
+print("I love playing badminton")
+
+#performing and printing a simple calculator
+print("the result of 3+5 is", 3+5)
